93e4cfd179d142524206d18a18360db80cb23d69
Tushar Mathur <tusharmath@gmail.com>
2018-07-22 17:15:21 +0530

    refactor(component): optimize imports
    affects: @action-land/component

--- modules/component/index.ts
+++ modules/component/index.ts
@@ -1,6 +1,5 @@
-import {Action} from '@action-land/core'
 import {Hoe} from '@action-land/smitten'
-import {ReducerFunction, CommandFunction} from '@action-land/tarz'
+import {CommandFunction, ReducerFunction} from '@action-land/tarz'
 
 export interface Component<State, Params, VNode> {
   init(p?: Partial<State>): State
