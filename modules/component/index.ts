2e0503f52149c0fda573273e3fa579f19dae5779
Tushar Mathur <tusharmath@gmail.com>
2018-08-02 13:58:19 +0530

    refactor(component): update typings for \`init()\`
    affects: @action-land/component
    
    enable init() to have multiple arguments
    
    BREAKING CHANGE:
    \`init()\` is not a unary function any more

--- modules/component/index.ts
+++ modules/component/index.ts
@@ -1,18 +1,12 @@
-import {create, Hoe} from '@action-land/smitten'
+import {create, Smitten} from '@action-land/smitten'
 import {CommandFunction, ReducerFunction} from '@action-land/tarz'
 
 /**
  * Component interface.
  */
-export interface Component<
-  State = {},
-  Params = {},
-  VNode = {},
-  StateParams = {}
-> {
-  init(): State
-  init(p: StateParams): State
+export interface Component<State = {}, Params = {}, VNode = {}> {
+  init<T extends any[]>(...t: T): State
   update: ReducerFunction<State>
   command: CommandFunction<State>
-  view(e: Hoe, m: State, p: Params): VNode
+  view(e: Smitten, m: State, p: Params): VNode
 }
