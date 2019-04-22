51da7124e5474ef2f004c3c917daea11a374bb80
Joel <jlouzado@gmail.com>
2019-04-22 16:27:17 +0530

    test(core): add test for isNil
    affects: @action-land/core

---
+++ test/core/isNil.ts
@@ -0,0 +1,11 @@
+import {action, isNil, Nil} from '@action-land/core'
+import * as assert from 'assert'
+
+describe('isNil', () => {
+  it('should return false if obj is not Nil action', () => {
+    assert.ok(!isNil(action('A', [10])))
+  })
+  it('should return true if action is a Nil Action', () => {
+    assert.ok(isNil(Nil()))
+  })
+})
