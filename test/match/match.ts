52e9b150cd64f59bd5f121eb0aa48d3f2244ceea
Joel <jlouzado@gmail.com>
2019-04-23 18:45:04 +0530

    style(test/match.ts): reorder object-keys alphabetically

--- test/match/match.ts
+++ test/match/match.ts
@@ -7,8 +7,8 @@ import {match} from '../../modules/match/index'
 describe('match', () => {
   it('should match based on action.type', () => {
     const actual = match((i: number) => i, {
-      inc: (i: number) => i + 1,
-      greet: (i: string) => `hi ${i}`
+      greet: (i: string) => `hi ${i}`,
+      inc: (i: number) => i + 1
     })(action('inc', 10))
     const expected = 11
     assert.strictEqual(actual, expected)
@@ -16,16 +16,16 @@ describe('match', () => {
 
   it('should call default with action if nothing matches', () => {
     const actual = match(i => i.value, {
-      inc: (i: number) => i + 1,
-      dec: (i: number) => i - 1
+      dec: (i: number) => i - 1,
+      inc: (i: number) => i + 1
     })(action('otherwise', 10))
     const expected = 10
     assert.strictEqual(actual, expected)
   })
   it('should be curried', () => {
     const actual = match(i => i.value)({
-      inc: (i: number) => i + 1,
-      dec: (i: number) => i - 1
+      dec: (i: number) => i - 1,
+      inc: (i: number) => i + 1
     })(action('otherwise', 10))
     const expected = 10
     assert.strictEqual(actual, expected)
@@ -33,8 +33,8 @@ describe('match', () => {
 
   it('should handle non-action types', () => {
     const actual = match(() => 1000, {
-      inc: (i: number) => i + 1,
-      dec: (i: number) => i - 1
+      dec: (i: number) => i - 1,
+      inc: (i: number) => i + 1
     })(null)
     const expected = 1000
     assert.strictEqual(actual, expected)
