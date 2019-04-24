011d1a878264ea7ba4e4fdd083a9a0f0a42bb760
Joel <jlouzado@gmail.com>
2019-04-24 15:40:23 +0530

    docs(jsdocs): tweak incorrect docs
    affects: @action-land/core, @action-land/tarz

--- modules/core/src/isAction.ts
+++ modules/core/src/isAction.ts
@@ -2,7 +2,6 @@ import {IAction} from './action'
 
 /**
  * Checks if the object is of Action type
- * TODO(tsc): Improve typings with overrides
  * @param: Any Object
  */
 export function isAction(obj: any): obj is IAction<any> {
