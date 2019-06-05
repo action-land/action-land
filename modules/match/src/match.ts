8369a21d36bf86ac9ca57169976a8db00ed1b6f5
Joel <jlouzado@gmail.com>
2019-06-05 14:13:39 +0530

    fix(match): improve typings
    affects: @action-land/match

--- modules/match/src/match.ts
+++ modules/match/src/match.ts
@@ -1,23 +1,24 @@
-import {IAction, isAction} from '@action-land/core'
+import {isAction} from '@action-land/core'
 import {CurriedFunction2, curry2} from 'ts-curry'
 
 import {hasOwnProperty} from '../../utils'
 
-// tslint:disable:no-any no-unsafe-any
+// tslint:disable:no-any
+
 /**
  * Defines structure of the spec that represents handlers for each action
  */
 export interface IMatchActionSpec {
-  [key: string]: (value: any) => any
+  [key: string]: (value: any) => unknown
 }
 
 export const match: CurriedFunction2<
-  (t: any) => any,
+  (t: unknown) => unknown,
   IMatchActionSpec,
   any
 > = curry2(
   (base: (t: unknown) => unknown, spec: IMatchActionSpec) => (
-    action: IAction<any>
+    action: unknown
   ) =>
     isAction(action) && hasOwnProperty(action.type, spec)
       ? spec[action.type](action.value)
--- test/match/match.ts
+++ test/match/match.ts
@@ -1,14 +1,15 @@
-import {action, IAction} from '@action-land/core'
+import {action, isAction} from '@action-land/core'
 import * as assert from 'assert'
 import {describe, it} from 'mocha'
 
 import {match} from '../../modules/match/index'
 
-// tslint:disable: no-unsafe-any no-null-keyword no-magic-numbers
+// tslint:disable:no-null-keyword no-magic-numbers
+// tslint:disable: no-unsafe-any
 
 describe('match', () => {
   it('should match based on action.type', () => {
-    const actual = match((i: number) => i, {
+    const actual = match((i: unknown) => i, {
       greet: (i: string) => `hi ${i}`,
       inc: (i: number) => i + 1
     })(action('inc', 10))
@@ -16,7 +17,7 @@ describe('match', () => {
     assert.strictEqual(actual, expected)
   })
   it('should call default with action if nothing matches', () => {
-    const actual = match((i: IAction<unknown>) => i.value, {
+    const actual = match((i: unknown) => (isAction(i) ? i.value : i), {
       dec: (i: number) => i - 1,
       inc: (i: number) => i + 1
     })(action('otherwise', 10))
@@ -24,7 +25,7 @@ describe('match', () => {
     assert.strictEqual(actual, expected)
   })
   it('should be curried', () => {
-    const actual = match((i: IAction<unknown>) => i.value)({
+    const actual = match((i: unknown) => (isAction(i) ? i.value : i))({
       dec: (i: number) => i - 1,
       inc: (i: number) => i + 1
     })(action('otherwise', 10))
